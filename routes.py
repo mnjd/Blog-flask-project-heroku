058115ba07b2ae64b417ea1fc523e6ea82b32262
manojd <manoj.ducher@gmail.com>
2018-02-28 11:20:50 +0100

    solved bug import jsonify

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from datetime import datetime
